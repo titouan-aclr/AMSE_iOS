d2156589774c2e42654cb4caea56eb4b2920881c
admin <admin@Mac-mini-de-admin-5.local>
2024-02-20 14:23:30 +0100

    Initial Commit

---
+++ jeu_2048/jeu_2048/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  jeu_2048
+//
+//  Created by admin on 20/02/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
